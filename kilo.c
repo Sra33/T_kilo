9bc3e5d0e3f316b7efce1b90c774bdbbaa520f77
Sra33 <salamia129@gmail.com>
2023-09-01 18:59:00 +0100

    added arrow up, down , left , write function to move cursor

--- kilo.c
+++ kilo.c
@@ -7,7 +7,17 @@
 #include <sys/ttydefaults.h>
 #include <unistd.h>
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
+};
+
 struct editorConfig {
+  int cx, cy;
   struct termios orig_termios;
   int screenrows;
   int screencols;
@@ -106,6 +116,8 @@ int getWindowSize(int *row, int *cols) {
  * initEditor - initialise all fields in the struct
  * */
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("get Windowsize");
 }
@@ -148,7 +160,7 @@ void enableRawMode() {
 /**
  * editorReadkey - It reads the keypress from keyboard
  */
-char editorReadKey() {
+int editorReadKey() {
   int nread;
   char c;
 
@@ -156,17 +168,101 @@ char editorReadKey() {
     if (nread == -1 && errno != EAGAIN)
       die("read");
   }
-  return c;
+
+  if (c == '\x1b') {
+    char seq[3];
+
+    if (read(STDIN_FILENO, &seq[0], 1) != 1)
+      return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1)
+      return '\x1b';
+
+    if (seq[0] == '[') {
+
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_UP;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+        case 'A':
+          return ARROW_UP;
+        case 'B':
+          return ARROW_DOWN;
+        case 'C':
+          return ARROW_RIGHT;
+        case 'D':
+          return ARROW_LEFT;
+        }
+      }
+    }
+
+    return '\x1b';
+  } else {
+    return c;
+  }
+}
+
+/*
+ * editorCursorMove - this function moves the editor with the wasd commands
+ * @key: char
+ */
+
+void editorCursorMove(unsigned int key) {
+  switch (key) {
+  case ARROW_LEFT:
+    if (E.cx != 0) {
+      E.cx--;
+    }
+    break;
+  case ARROW_RIGHT:
+    if (E.cx != E.screencols - 1) {
+      E.cx++;
+    }
+    break;
+  case ARROW_DOWN:
+    if (E.cy != 0) {
+      E.cy--;
+    }
+    break;
+  case ARROW_UP:
+    if (E.cy != E.screenrows - 1) {
+      E.cy++;
+    }
+    break;
+  }
 }
 
 void editorProcessKeyPress() {
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch (c) {
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
+    break;
+
+  case PAGE_UP:
+  case PAGE_DOWN: {
+    int times = E.screenrows;
+    while (times--) {
+      editorCursorMove(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+  }
+
+  case ARROW_UP:
+  case ARROW_DOWN:
+  case ARROW_LEFT:
+  case ARROW_RIGHT:
+    editorCursorMove(c);
     break;
   }
 }
@@ -206,7 +302,10 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6); // it is used for showing cursor
 
   write(STDOUT_FILENO, ab.b, ab.len);
